f9c3ddbd4018a20f9406d7740795e6b489cdfdc0
belaoumu <belaoun.mustapha@outlook.de>
2025-11-14 13:22:50 +0100

    14.11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+   DatabaseConnector DC= new DatabaseConnector("com.mysql.jdbc.Driver",pDatabase:);
+)
     }
 }
